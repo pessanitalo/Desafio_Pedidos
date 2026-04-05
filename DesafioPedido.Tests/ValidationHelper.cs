243161b59ef9803969d8a2d04b543b370310c787
Italo Pessan <italopessan@gmail.com>
2026-04-05 15:56:11 -0300

    classe auxiliar de validação

---
+++ DesafioPedido.Tests/ValidationHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesafioPedido.Tests
+{
+    public static class ValidationHelper
+    {
+        public static List<ValidationResult> ValidarTests(object model)
+        {
+            var context = new ValidationContext(model);
+            var results = new List<ValidationResult>();
+
+            Validator.TryValidateObject(model, context, results, true);
+
+            return results;
+        }
+    }
+}
